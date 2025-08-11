6e3addf360427eb8ec1e73f1071ac8130836f5c0
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2025-08-11 17:05:43 +0200

    feat(core): add single path param extractor

--- packages/core/src/extract/path.ts
+++ packages/core/src/extract/path.ts
@@ -12,6 +12,60 @@ export class InvalidPathParamsError extends ValidationError {
 }
 
 /**
+ * Extractor that will get a single path param from the URL and parse it.
+ *
+ * The schema can be anything implementing the [Standard Schema](https://standardschema.dev/).
+ *
+ * If your path contains more than one value, you must use {@link pathParams}
+ * instead.
+ *
+ * If the param cannot be parsed, it will reject the request with a
+ * `400 Bad Request` response.
+ *
+ * @example
+ * ```ts
+ * import { pathParam } from "@taxum/core/extract";
+ * import { m, Router } from "@taxum/core/routing";
+ * import { z } from "zod";
+ *
+ * const handler = handler([pathParam(z.uuid())], ({id}) => {
+ *     // ...
+ * });
+ *
+ * const router = new Router()
+ *     .route("/users/:id", m.get(handler));
+ * ```
+ *
+ * @throws {@link !Error} if path params have more than one value.
+ * @throws {@link InvalidPathParamsError} if the params cannot be parsed.
+ */
+export const pathParam =
+    <T extends StandardSchemaV1>(schema: T): Extractor<StandardSchemaV1.InferOutput<T>> =>
+    async (req: HttpRequest): Promise<StandardSchemaV1.InferOutput<T>> => {
+        const pathParams = req.extensions.get(PATH_PARAMS);
+        assert(pathParams, "Path params not found. Did you forget to use the path router?");
+
+        const paramEntries = Object.entries(pathParams);
+        assert(paramEntries.length === 1, "Path params must have exactly one value");
+
+        const [paramName, paramValue] = paramEntries[0];
+
+        const parseResult = await schema["~standard"].validate(paramValue);
+
+        if (parseResult.issues) {
+            throw new InvalidPathParamsError(
+                parseResult.issues.map((issue) => ({
+                    ...issue,
+                    /* node:coverage ignore next */
+                    path: issue.path === undefined ? [paramName] : [paramName, ...issue.path],
+                })),
+            );
+        }
+
+        return parseResult.value;
+    };
+
+/**
  * Extractor that will get path params from the URL and parse them.
  *
  * The schema can be anything implementing the [Standard Schema](https://standardschema.dev/).
--- packages/core/test/extract/path.test.ts
+++ packages/core/test/extract/path.test.ts
@@ -2,53 +2,117 @@ import assert from "node:assert/strict";
 import consumers from "node:stream/consumers";
 import { describe, it } from "node:test";
 import { z } from "zod";
-import { InvalidPathParamsError, pathParams } from "../../src/extract/index.js";
+import { InvalidPathParamsError, pathParam, pathParams } from "../../src/extract/index.js";
 import { HttpRequest, StatusCode } from "../../src/http/index.js";
 import { PATH_PARAMS } from "../../src/routing/index.js";
 
 describe("extract:path", () => {
-    const schema = z.object({
-        userId: z.coerce.number().int().positive(),
+    describe("pathParam", () => {
+        const schema = z.coerce.number().int().positive();
+
+        it("parses valid path parameter", async () => {
+            const req = HttpRequest.builder().extension(PATH_PARAMS, { userId: "42" }).body(null);
+
+            const extract = pathParam(schema);
+            const result = await extract(req);
+
+            assert.equal(result, 42);
+        });
+
+        it("throws assertion error if path params extension is missing", async () => {
+            const req = HttpRequest.builder().body(null);
+
+            const extract = pathParam(schema);
+
+            await assert.rejects(
+                async () => extract(req),
+                (err: unknown) => {
+                    assert.ok(err instanceof assert.AssertionError);
+                    assert.match((err as Error).message, /Path params not found/);
+                    return true;
+                },
+            );
+        });
+
+        it("throws assertion error if path params have more than one value", async () => {
+            const req = HttpRequest.builder()
+                .extension(PATH_PARAMS, { userId: "42", clientId: "69" })
+                .body(null);
+
+            const extract = pathParam(schema);
+
+            await assert.rejects(
+                async () => extract(req),
+                (err: unknown) => {
+                    assert.ok(err instanceof assert.AssertionError);
+                    assert.match((err as Error).message, /Path params must have exactly one value/);
+                    return true;
+                },
+            );
+        });
+
+        it("throws InvalidPathParamsError for invalid path param", async () => {
+            const req = HttpRequest.builder().extension(PATH_PARAMS, { userId: "-10" }).body(null);
+
+            const extract = pathParam(schema);
+
+            await assert.rejects(
+                async () => extract(req),
+                (err: unknown) => {
+                    assert(err instanceof InvalidPathParamsError);
+                    assert(Array.isArray(err.issues));
+                    assert(err.issues.length > 0);
+                    assert(err.issues[0].path[0] === "userId");
+                    return true;
+                },
+            );
+        });
     });
 
-    it("parses valid path parameters", async () => {
-        const req = HttpRequest.builder().extension(PATH_PARAMS, { userId: "42" }).body(null);
+    describe("pathParams", () => {
+        const schema = z.object({
+            userId: z.coerce.number().int().positive(),
+        });
 
-        const extract = pathParams(schema);
-        const result = await extract(req);
+        it("parses valid path parameters", async () => {
+            const req = HttpRequest.builder().extension(PATH_PARAMS, { userId: "42" }).body(null);
 
-        assert.deepEqual(result, { userId: 42 });
-    });
+            const extract = pathParams(schema);
+            const result = await extract(req);
 
-    it("throws assertion error if path params extension is missing", async () => {
-        const req = HttpRequest.builder().body(null);
+            assert.deepEqual(result, { userId: 42 });
+        });
 
-        const extract = pathParams(schema);
+        it("throws assertion error if path params extension is missing", async () => {
+            const req = HttpRequest.builder().body(null);
 
-        await assert.rejects(
-            async () => extract(req),
-            (err: unknown) => {
-                assert.ok(err instanceof assert.AssertionError);
-                assert.match((err as Error).message, /Path params not found/);
-                return true;
-            },
-        );
-    });
+            const extract = pathParams(schema);
 
-    it("throws InvalidPathParamsError for invalid path params", async () => {
-        const req = HttpRequest.builder().extension(PATH_PARAMS, { userId: "-10" }).body(null);
+            await assert.rejects(
+                async () => extract(req),
+                (err: unknown) => {
+                    assert(err instanceof assert.AssertionError);
+                    assert.match((err as Error).message, /Path params not found/);
+                    return true;
+                },
+            );
+        });
 
-        const extract = pathParams(schema);
+        it("throws InvalidPathParamsError for invalid path params", async () => {
+            const req = HttpRequest.builder().extension(PATH_PARAMS, { userId: "-10" }).body(null);
 
-        await assert.rejects(
-            async () => extract(req),
-            (err: unknown) => {
-                assert.ok(err instanceof InvalidPathParamsError);
-                assert.ok(Array.isArray(err.issues));
-                assert.ok(err.issues.length > 0);
-                return true;
-            },
-        );
+            const extract = pathParams(schema);
+
+            await assert.rejects(
+                async () => extract(req),
+                (err: unknown) => {
+                    assert(err instanceof InvalidPathParamsError);
+                    assert(Array.isArray(err.issues));
+                    assert(err.issues.length > 0);
+                    return true;
+                },
+            );
+        });
     });
 
     it("InvalidPathParamsError produces 400 response", async () => {
